f6ecbcd4eb2bc2059047848f2b4861d4566b4e78
minhtuandev65 <huynhminhtuan13032003@gmail.com>
2025-05-13 06:22:46 +0700

    update

--- src/Utils/Setting/Config.jsx
+++ src/Utils/Setting/Config.jsx
@@ -1,15 +1,14 @@
-let apiRoot = ''
+let apiRoot = "";
 
-
-if (process.env.BUILD_MODE === 'dev') {
-  apiRoot = 'http://localhost:1303/v1'
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:1303/v1";
 }
 
-if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://team-job-seek-api.onrender.com/v1'
+if (process.env.BUILD_MODE === "production") {
+  apiRoot = "https://project-ticketbox-api.onrender.com/v1";
 }
 
-export const DOMAIN = apiRoot
+export const DOMAIN = apiRoot;
 export const USER_LOGIN = "USER_LOGIN";
 export const TOKEN = "ACCESS_TOKEN";
 export const REFRESH_TOKEN = "REFRESH_TOKEN";
